594a47b305d98c4e273661b561e535e80c9a343b
Greg Steiert <greg.steiert@latticesemi.com>
2019-09-12 13:12:10 -0700

    updated header for machXOi2c.ino

--- examples/machXOi2c/machXOi2c.ino
+++ examples/machXOi2c/machXOi2c.ino
@@ -1,13 +1,25 @@
-/*********************************************************************
- Adafruit invests time and resources providing this open source code,
- please support Adafruit and open-source hardware by purchasing
- products from Adafruit!
-
- MIT license, check LICENSE for more information
- Copyright (c) 2019 Ha Thach for Adafruit Industries
- All text above, and the splash screen below must be included in
- any redistribution
-*********************************************************************/
+// MIT License
+//
+// Author(s): Greg Steiert
+// Copyright (c) 2019 Lattice Semiconductor Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 /* This example shows how to access the configuration port in a Lattice MachXO2/3
  *   device.  It utilizes the TinyUSB from Adafruit to expose on-board external Flash 
@@ -22,6 +34,8 @@
  * Note: Adafruit fork of SdFat enabled ENABLE_EXTENDED_TRANSFER_CLASS and FAT12_SUPPORT
  * in SdFatConfig.h, which is needed to run SdFat on external flash. You can use original
  * SdFat library and manually change those macros
+ * 
+ * This code uses pieces of the Adafruit TinyUSB examples for the file access
  */
 
 #include "SPI.h"
